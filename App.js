7356d99ff41799b52263e3ee72d934fa640e8153
Redouane DRAYEF <69579923+rdrayef@users.noreply.github.com>
2021-10-03 16:51:48 +0100

    Uber eats clone using react native

---
+++ App.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { View } from "react-native";
+import RootNavigation from "./navigation";
+import Home from "./Screens/Home";
+import RestaurantDetails from "./Screens/RestaurantDetails";
+
+export default function App() {
+
+  return (
+    <RootNavigation />
+  );
+
+}
